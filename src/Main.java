ce3a83d77f67127023123b649c2b71ef505bd4d0
Ivan Altamirano <pedrotrill@gmail.com>
2023-12-21 09:21:19 -0300

    agrego variable ability

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import Poke.Pokemon;
 public class Main {
     public static void main(String[] args) {
-        Pokemon pikachu = new Pokemon("Pikachu", "Eléctrico", 4);
+        Pokemon pikachu = new Pokemon("Pikachu", "Eléctrico", 4, "Thunder Shock");
         System.out.println(pikachu);
         pikachu.attack();
         pikachu.evolve();
--- src/Poke/Pokemon.java
+++ src/Poke/Pokemon.java
@@ -4,22 +4,24 @@ public class Pokemon {
     private String name;
     private String type;
     private int level;
+    private String ability;
 
-    public Pokemon(String name, String type, int level) {
+    public Pokemon(String name, String type, int level, String ability) {
         this.name = name;
         this.type = type;
         this.level = level;
+        this.ability = ability;
     }
     public void attack() {
-        System.out.println(name + " ataca con un Thunder Shock!");
+        System.out.println(name + " ataca con un " + ability + "!");
     }
     public void evolve() {
-        System.out.println("Falta expe para evolucionar " + name + ". A seguir bicheando!");
+        System.out.println("Falta expe para evolucionar. A seguir bicheando " + name + "!");
     }
 
     @Override
     public String toString() {
         return "Pokemon {" +
-                "\nnombre= " + name + ", \ntipo= " + type + ", \nnivel= " + level + "\n}";
+                "\nnombre= " + name + ", \ntipo= " + type + ", \nnivel= " + level + ", \nhabilidad= " + ability + "\n}";
     }
 }
